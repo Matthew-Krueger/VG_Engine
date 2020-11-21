9cbe5181627dec5fa5b86acf2fe0c2a53ba141f6
Matthew Kruger <matthapkidokarate@gmail.com>
2020-11-20 19:03:49 -0500

    Add project files.

---
+++ VG_Engine/VG_Engine.cpp
@@ -0,0 +1,12 @@
+﻿// VG_Engine.cpp : Defines the entry point for the application.
+//
+
+#include "VG_Engine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
